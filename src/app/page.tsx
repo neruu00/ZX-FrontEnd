aadc6f5229b807e08bd88cbe72c4b5568ad8a6de
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-11-25 21:13:18 +0900

    feat: add button component and update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { ThemeToggle } from '@/components/ThemeToggle';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="bg-background">
+      <h1>Home</h1>
+      <div className="flex flex-col gap-4 w-[180px] p-4">
+        <Button variant="default">독후감 작성</Button>
+        <Button variant="outline">독후감 작성</Button>
+        <Button variant="ghost">독후감 작성</Button>
+        <Button variant="highlight">독후감 작성</Button>
+      </div>
+      <ThemeToggle />
+    </main>
+  );
 }
